8f948179cfeb09083eb91f88aee7fda4b110cf29
Admin <mdd.group08@gmail.com>
2021-10-25 18:10:15 +0800

    edited login page, signup page, added utilities and loginbutton dart files

---
+++ flutter_breathalyzer_finaldemo/lib/Component/loginbutton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final String title;
+  final dynamic  ontapp;
+
+  LoginButton({required this.title, required this.ontapp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed:
+        ontapp,
+
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text(
+          'LOGIN',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }}
