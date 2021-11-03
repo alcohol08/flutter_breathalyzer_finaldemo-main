044699cc23853fd3ca8be605d383eb806d122bdc
alcohol08 <90031865+alcohol08@users.noreply.github.com>
2021-11-03 21:34:50 +0800

    Add IntroScreens before LoginScreen

--- flutter_breathalyzer_finaldemo/lib/Screens/Introscreen.dart
+++ flutter_breathalyzer_finaldemo/lib/Screens/Introscreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'login.dart';
+//! sws
 
 class IntroScreen extends StatefulWidget {
 
